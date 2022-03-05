8b7054c3aa7e94f9e8509c22a02cd87d985fcb22
Adam Dernis <avid29@live.com>
2022-03-05 14:41:38 -0500

    AudioOutputNode

---
+++ src/ModSynth.Graph/Nodes/Output/AudioOutputNode.cs
@@ -0,0 +1,21 @@
+﻿using ModSynth.Common;
+using ModSynth.Graph.Nodes.Interfaces;
+using ModSynth.Graph.Ports;
+
+namespace ModSynth.Graph.Nodes.Output
+{
+    public class AudioOutputNode : INode
+    {
+        public AudioOutputNode()
+        {
+            InPort = new InPort<AudioFrame>(this);
+        }
+
+        public InPort<AudioFrame> InPort { get; }
+
+        public void Execute(AudioFrame frame)
+        {
+            InPort.Execute(frame);
+        }
+    }
+}
