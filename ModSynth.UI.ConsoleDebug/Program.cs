2a376b05de0a70c266e7400a92eece597840d7b2
Adam Dernis <avid29@live.com>
2022-03-05 14:55:35 -0500

    Basic console debug

---
+++ ModSynth.UI.ConsoleDebug/Program.cs
@@ -0,0 +1,21 @@
+﻿using ModSynth.UI.WinUI.Rendering;
+using System;
+
+public class Program
+{
+    public async static void MainAsync()
+    {
+        AudioGraphRenderer renderer = new AudioGraphRenderer();
+        Console.WriteLine("Initializing AudioGraph...");
+        await renderer.InitializeAsync();
+        Console.WriteLine("Done");
+        Console.WriteLine("Playing...");
+        renderer.Play();
+    }
+
+    public static void Main()
+    {
+        MainAsync();
+        Console.ReadLine();
+    }
+}
