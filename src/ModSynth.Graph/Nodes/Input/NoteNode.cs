e33f841963d8e2ad14a5396653873a5cb3b4ee99
Adam Dernis <avid29@live.com>
2022-03-05 14:40:38 -0500

    Note and Tuning Nodes

---
+++ src/ModSynth.Graph/Nodes/Input/NoteNode.cs
@@ -0,0 +1,37 @@
+﻿using ModSynth.Common;
+using ModSynth.Common.Enums;
+using ModSynth.Common.Models;
+using ModSynth.Graph.Nodes.Interfaces;
+using ModSynth.Graph.Ports;
+
+namespace ModSynth.Graph.Nodes.Input
+{
+    public class NoteNode : INode
+    {
+        private Note _note = new Note(NoteName.A, 4);
+
+        public NoteNode()
+        {
+            OutPort = new OutPort<Note>(this);
+        }
+
+        public NoteName Note
+        {
+            get => _note.NoteName;
+            set => _note.NoteName = value;
+        }
+
+        public int Octave
+        {
+            get => _note.Octave;
+            set => _note.Octave = value;
+        }
+
+        public OutPort<Note> OutPort { get; }
+
+        public void Execute(AudioFrame frame)
+        {
+            OutPort.Value = _note;
+        }
+    }
+}
---
+++ src/ModSynth.Graph/Nodes/Input/TuningNode.cs
@@ -0,0 +1,37 @@
+﻿using ModSynth.Common;
+using ModSynth.Common.Enums;
+using ModSynth.Common.Models;
+using ModSynth.Graph.Nodes.Interfaces;
+using ModSynth.Graph.Ports;
+
+namespace ModSynth.Graph.Nodes.Input
+{
+    public class TuningNode : INode
+    {
+        private Tuning _tuning = new Tuning(TuningType.EqualTempered, new Note(NoteN.A, 4), 440);
+
+        public TuningNode()
+        {
+            OutPort = new OutPort<Tuning>(this);
+        }
+
+        public Note BasisNote
+        {
+            get => _tuning.BasisNote;
+            set => _tuning.BasisNote = value;
+        }
+
+        public float BasisFrequency
+        {
+            get => _tuning.BasisFrequency;
+            set => _tuning.BasisFrequency = value;
+        }
+
+        public OutPort<Tuning> OutPort { get; }
+
+        public void Execute(AudioFrame frame)
+        {
+            OutPort.Value = _tuning;
+        }
+    }
+}
