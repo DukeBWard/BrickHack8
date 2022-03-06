9651e4490f5be54a3d3a867d8eca734b18d8c5ec
Adam Dernis <avid29@live.com>
2022-03-05 20:46:21 -0500

    Simplified NoteNode

--- src/ModSynth.Graph/Nodes/Input/NoteNode.cs
+++ src/ModSynth.Graph/Nodes/Input/NoteNode.cs
@@ -8,30 +8,18 @@ namespace ModSynth.Graph.Nodes.Input
 {
     public class NoteNode : INode
     {
-        private Note _note = new Note(NoteName.A, 4);
-
         public NoteNode()
         {
             OutPort = new OutPort<Note>(this);
         }
 
-        public NoteName Note
-        {
-            get => _note.NoteName;
-            set => _note.NoteName = value;
-        }
-
-        public int Octave
-        {
-            get => _note.Octave;
-            set => _note.Octave = value;
-        }
+        public Note Note { get; set; } = new Note(NoteName.A, 4);
 
         public OutPort<Note> OutPort { get; }
 
         public void Execute(AudioFrame frame)
         {
-            OutPort.Value = _note;
+            OutPort.Value = Note;
         }
     }
 }
