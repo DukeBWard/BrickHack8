2182493e7e27233926d3964e9e96d3dfe88aa48d
Domenic Cacace <DMC1542@rit.edu>
2022-03-05 21:04:58 -0500

    Created TuningNode view model, its is barebones. Actually created noteNode in NoteNodeViewModel

---
+++ src/UI/ModSynth.ViewModels/ViewModels/TuningNodeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ModSynth.Common.Models;
+using ModSynth.Graph.Nodes.Input;
+
+namespace ModSynth.ViewModels
+{
+    internal class TuningNodeViewModel : ObservableObject
+    {
+        TuningNode _tuningNode;
+
+        public TuningNodeViewModel()
+        {
+        }
+
+        public string NoteString
+        {
+            get => _noteString;
+            set
+            {
+                bool success = Note.TryParse(value, out Note note);
+                if (!success) return;
+                SetProperty(ref _noteString, value);
+                _noteNode.Note = note;
+            }
+        }
+    }
+}
