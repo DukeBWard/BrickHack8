a8504e693bcfbe5b7931334664050510dff270d0
Domenic Cacace <DMC1542@rit.edu>
2022-03-05 22:28:17 -0500

    Created MixPCMNodeViewModel to mix two waves. can interact with both the node and its two waves

---
+++ src/UI/ModSynth.ViewModels/ViewModels/MixPCMNodeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ModSynth.Graph.Nodes.PCM;
+using ModSynth.Common;
+
+namespace ModSynth.ViewModels.ViewModels
+{
+    public class MixPCMNodeViewModel : ObservableObject
+    {
+        MixPCMNode _mPCMNode;
+        AudioFrame _waveA, _waveB;
+
+        public MixPCMNodeViewModel()
+        {
+            _mPCMNode = new MixPCMNode();
+        }
+
+        public MixPCMNode MixPCMNode 
+        { 
+            get => _mPCMNode;
+        }
+
+        public AudioFrame WaveFormA
+        {
+            get => _waveA;
+            set
+            {
+                _mPCMNode.WaveInPortA.Fallback = value;
+                _waveA = value;
+            }
+        }
+
+        public AudioFrame WaveFormB
+        {
+            get => _waveB;
+            set
+            {
+                _mPCMNode.WaveInPortB.Fallback = value;
+                _waveB = value;
+            }
+        }
+    }
+}
