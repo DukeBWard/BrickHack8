e23d7a1d6db1181d674723e698ba74a62e4be1ba
Adam Dernis <avid29@live.com>
2022-03-05 21:44:50 -0500

    Added UseA and UseB properties

--- src/ModSynth.Graph/Nodes/PCM/MixPCMNode.cs
+++ src/ModSynth.Graph/Nodes/PCM/MixPCMNode.cs
@@ -15,7 +15,11 @@ namespace ModSynth.Graph.Nodes.PCM
 
         public InPort<AudioFrame> WaveInPortA { get; set; }
 
+        public bool UseA { get; set; }
+
         public InPort<AudioFrame> WaveInPortB { get; set; }
+
+        public bool UseB { get; set; }
 
         public OutPort<AudioFrame> WaveOutPort { get; }
 
@@ -26,7 +30,9 @@ namespace ModSynth.Graph.Nodes.PCM
 
             for (int i = 0; i < frame.Samples; i++)
             {
-                frame.Payload[i] = a.Payload[i] + b.Payload[i];
+                frame.Payload[i] = 0;
+                if (UseA) frame.Payload[i] = a.Payload[i];
+                if (UseB) frame.Payload[i] = b.Payload[i];
             }
 
             WaveOutPort.Value = frame;
